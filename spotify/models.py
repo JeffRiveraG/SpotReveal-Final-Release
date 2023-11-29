593e8ed8244ec78656f4b718bbae3888cf93f4f9
Jeffrey Rivera <rjeffrey0929@gmail.com>
2023-11-28 16:17:35 -0800

    Added to the models database and recommended playlist features

--- spotify/models.py
+++ spotify/models.py
@@ -1,3 +1,10 @@
+# spotify/models.py
+
 from django.db import models
 
-# Create your models here.
+class SpotifyUser(models.Model):
+    spotify_id = models.CharField(max_length=255, unique=True)
+    access_token = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.spotify_id
--- spotify/views.py
+++ spotify/views.py
@@ -3,11 +3,12 @@
 from django.shortcuts import render, redirect
 from spotipy import Spotify
 from spotipy.oauth2 import SpotifyOAuth
+from .models import SpotifyUser
 
 SPOTIPY_CLIENT_ID = '4eb37ee19dd14e2796b4619a9470b0a0'
 SPOTIPY_CLIENT_SECRET = 'ec957ceb84b54da9b3ccecde98ce59f4'
 SPOTIPY_REDIRECT_URI = 'http://localhost:8000/callback'
-SPOTIPY_SCOPE = 'user-top-read'
+SPOTIPY_SCOPE = 'user-top-read playlist-modify-public playlist-read-private'
 
 sp_oauth = SpotifyOAuth(SPOTIPY_CLIENT_ID, SPOTIPY_CLIENT_SECRET, SPOTIPY_REDIRECT_URI, scope=SPOTIPY_SCOPE)
 
@@ -19,23 +20,62 @@ def login(request):
     return redirect(auth_url)
 
 def callback(request):
-    token_info = sp_oauth.get_access_token(request.GET.get('code', ''))
-    request.session['token_info'] = token_info
-    return redirect('top_combined')
+    code = request.GET.get('code', '')
+    token_info = sp_oauth.get_access_token(code)
 
-def top_combined(request):
-    token_info = request.session.get('token_info', None)
-
-    if not token_info:
+    if 'access_token' not in token_info:
         return redirect('login')
 
     access_token = token_info['access_token']
     sp = Spotify(auth=access_token)
 
+    # Fetch user information
+    user_info = sp.current_user()
+
+    # Check if the user is already in the database
+    user, created = SpotifyUser.objects.get_or_create(spotify_id=user_info['id'])
+
+    # Update or create the SpotifyUser instance with the new access token
+    user.access_token = access_token
+    user.save()
+
+    request.session['user_id'] = user.id  # Store the user ID in the session
+
+    return redirect('top_combined')
+
+def top_combined(request):
+    user_id = request.session.get('user_id')
+
+    if not user_id:
+        return redirect('login')
+
+    try:
+        user = SpotifyUser.objects.get(id=user_id)
+    except SpotifyUser.DoesNotExist:
+        return redirect('login')
+
+    access_token = user.access_token
+    sp = Spotify(auth=access_token)
+
     # Fetch top tracks
-    top_tracks = sp.current_user_top_tracks(limit=10, time_range='short_term')
+    top_tracks = sp.current_user_top_tracks(limit=5, time_range='short_term')
 
     # Fetch top artists
-    top_artists = sp.current_user_top_artists(limit=10, time_range='short_term')
+    top_artists = sp.current_user_top_artists(limit=5, time_range='short_term')
 
-    return render(request, 'spotify/top_combined.html', {'tracks': top_tracks['items'], 'artists': top_artists['items']})
+    # Extract track IDs from top tracks
+    top_track_ids = [track['id'] for track in top_tracks['items']]
+
+    # Get recommendations based on top tracks
+    recommendations = sp.recommendations(seed_tracks=top_track_ids, limit=10)
+
+    # Create a playlist
+    playlist_name = "Recommended Playlist"
+    playlist_description = "A playlist generated based on your top tracks"
+    playlist = sp.user_playlist_create(user=sp.current_user()['id'], name=playlist_name, description=playlist_description)
+
+    # Add recommended tracks to the playlist
+    track_uris = [track['uri'] for track in recommendations['tracks']]
+    sp.playlist_add_items(playlist_id=playlist['id'], items=track_uris)
+
+    return render(request, 'spotify/top_combined.html', {'tracks': top_tracks['items'], 'artists': top_artists['items'], 'playlist_url': playlist['external_urls']['spotify'], 'recommendations': recommendations['tracks']})
